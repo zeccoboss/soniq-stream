3767428c6718309c814bdbbaace2f315375a3305
ZECCO <zeccoboss@hotmail.com>
2026-05-19 23:37:34 +0100

    Added the port from process evn in the server

--- server/src/config/app.config.js
+++ server/src/config/app.config.js
@@ -2,7 +2,7 @@ const { v4: uuidV4 } = require("uuid");
 class AppConfig {
 	#appName = "SoniqStream";
 
-	#port = process.env.NODE_ENV === "production" ? 3500 : 3500;
+	#port = process.env.NODE_ENV === "production" ? process.env.PORT : 3500;
 	// #port = process.env.NODE_ENV === "production" ? process.env.PORT : 3500;
 	#client_base_url = "https://zecco-music-app.vercel.app/";
 	#client_dev_url = `http://localhost:5173/`;
