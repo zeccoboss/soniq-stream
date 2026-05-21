9b1d7f20b7aa7afe2c39b180f2134c3e5fc1027f
ZECCO <zeccoboss@hotmail.com>
2026-05-21 07:26:00 +0100

    Add frontend production url to the backend whitelist

--- server/src/config/allowed-origins.config.js
+++ server/src/config/allowed-origins.config.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5174", "http://localhost:5173"];
+const allowedOrigins = [
+	"http://localhost:5174",
+	"http://localhost:5173",
+	"https://soniq-stream.vercel.app/",
+];
 
 module.exports = { allowedOrigins };
